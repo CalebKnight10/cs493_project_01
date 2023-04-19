8b10e3d480862e603ddae33bcc7b6b9d5f33e20a
Caleb Knight <knightca@oregonstate.edu>
2023-04-19 15:01:21 -0700

    Added app.js file with basic business structure

---
+++ app.js
@@ -0,0 +1,25 @@
+// Server work
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+const jsonParser = bodyParser.json();
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
+const business =  [
+    {
+        name: "",
+        address: "",
+        city: "",
+        state: "", 
+        zip: "",
+        phone: "",
+        type: "",
+        email: "",
+        website: ""
+    }
+]
